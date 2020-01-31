b6c06ea5c61c62f3183705c274f60426c6bdc607
AmineZnaiguia <znaiguiamohamedamine@gmail.com>
2020-01-31 18:38:00 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+
+const connectDB = require('./config/db');
+const user = require('./routes/users')
+
+
+const app = express();
+
+
+
+// Init middleware
+app.use(express.json());
+
+
+//connect database
+connectDB();
+//Define routes
+app.use('/', user);
+app.use('/auth', require('./routes/auth'));
+// app.use('/profile', require('./routes/profile'));
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`server is runnig on Port ${PORT}`))
